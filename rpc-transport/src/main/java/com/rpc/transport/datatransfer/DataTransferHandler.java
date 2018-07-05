a383c181804a62cce94995f6ef130bcb78a02d1f
jibj <jibj@Ctrip.com>
2018-07-05 21:46:03 +0800

    update

--- rpc-transport/src/main/java/com/rpc/transport/datatransfer/DataTransferHandler.java
+++ rpc-transport/src/main/java/com/rpc/transport/datatransfer/DataTransferHandler.java
@@ -1,15 +1,29 @@
 package com.rpc.transport.datatransfer;
 
+import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
 
 /**
  * @Author: Bojun Ji
  * @Date: Created in 2018-07-02 19:54
  * @Description:
  */
-public class DataTransferHandler extends SimpleChannelInboundHandler {
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
+public class DataTransferHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String body=(String)msg;
+    }
 
+    public void setApplicationContext() throws  Exception{
+
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        EventLoopGroup bossGroup=new NioEventLoopGroup();
+        EventLoopGroup workerGroup=new NioEventLoopGroup();
+        ServerBootstrap b=new ServerBootstrap();
     }
 }
