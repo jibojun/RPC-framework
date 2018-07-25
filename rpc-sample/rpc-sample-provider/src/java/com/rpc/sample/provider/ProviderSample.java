0e930a9d959d4769c5ec0aa0b772e822d56e71a8
jibj <jibj@Ctrip.com>
2018-07-25 12:17:50 +0800

    test

---
+++ rpc-sample/rpc-sample-provider/src/java/com/rpc/sample/provider/ProviderSample.java
@@ -0,0 +1,12 @@
+package com.rpc.sample.provider;
+
+/**
+ * @Author: Bojun Ji
+ * @Date: Created in 2018-07-25 12:13
+ * @Description:
+ */
+public class ProviderSample {
+    public static void main(String[] args) {
+        
+    }
+}
