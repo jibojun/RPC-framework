708fc056c5f8e4fb8dd95a5949cb33ac637c53c6
jibojun <jbj19910123@163.com>
2018-08-25 01:02:59 +0800

    update

--- rpc-sample/rpc-sample-provider/src/main/java/com/rpc/sample/provider/ProviderSample.java
+++ rpc-sample/rpc-sample-provider/src/main/java/com/rpc/sample/provider/ProviderSample.java
@@ -1,5 +1,14 @@
 package com.rpc.sample.provider;
 
+import com.rpc.api.ITestService;
+import com.rpc.common.entity.ServiceNameBeanEntity;
+import com.rpc.server.RPCServer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: Bojun Ji
  * @Date: Created in 2018-07-25 12:13
@@ -7,6 +16,11 @@ package com.rpc.sample.provider;
  */
 public class ProviderSample {
     public static void main(String[] args) {
-        
+        ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+        ((ClassPathXmlApplicationContext) context).start();
+        ITestService serviceBean=(ITestService)context.getBean("testService");
+        List<ServiceNameBeanEntity> serviceList=new ArrayList<>();
+        serviceList.add(new ServiceNameBeanEntity("testService",serviceBean));
+        RPCServer.exportAllServices(serviceList);
     }
 }
--- rpc-sample/rpc-sample-provider/src/main/java/com/rpc/sample/provider/impl/TestService.java
+++ rpc-sample/rpc-sample-provider/src/main/java/com/rpc/sample/provider/impl/TestService.java
@@ -1,15 +1,32 @@
 package com.rpc.sample.provider.impl;
 
 import com.rpc.api.ITestService;
+import org.springframework.stereotype.Component;
 
 /**
  * @Author: Bojun Ji
  * @Description:
  * @Date: 2018/8/25_12:09 AM
  */
+@Component("testService")
 public class TestService implements ITestService {
     @Override
     public String testService(int i) {
-        return null;
+        String result;
+        switch (i) {
+            case 1:
+                result = "hit 1";
+                break;
+            case 2:
+                result = "hit 2";
+                break;
+            case 3:
+                result = "hit 3";
+                break;
+            default:
+                result = "hit nothing";
+                break;
+        }
+        return result;
     }
 }
