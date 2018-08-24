efcc115c85dae74cb17ee5898f0d3c9d1d486ee0
jibojun <jbj19910123@163.com>
2018-08-25 00:10:30 +0800

    update

---
+++ rpc-sample/rpc-sample-provider/src/main/java/com/rpc/sample/provider/impl/TestService.java
@@ -0,0 +1,15 @@
+package com.rpc.sample.provider.impl;
+
+import com.rpc.api.ITestService;
+
+/**
+ * @Author: Bojun Ji
+ * @Description:
+ * @Date: 2018/8/25_12:09 AM
+ */
+public class TestService implements ITestService {
+    @Override
+    public String testService(int i) {
+        return null;
+    }
+}
